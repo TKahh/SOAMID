c21018a155d4b2a6e2d630f27ed3868b19c1cbd0
Ky Anh <trankyanh202@gmail.com>
2024-03-08 14:02:11 +0700

    Adding item to cart

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import datetime
 import re
 from flask import Flask, render_template
+from flask import request, jsonify, session
+from flask_session import Session
 import firebase_admin
 from firebase_admin import credentials, db, storage
 
 app = Flask(__name__, static_folder='static')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = '09120125'
+Session(app)
 cred = credentials.Certificate("soap-df2ab-firebase-adminsdk-u7cvg-1f48e561a6.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://soap-df2ab-default-rtdb.asia-southeast1.firebasedatabase.app/',
@@ -16,15 +21,37 @@ bucket = storage.bucket()
 
 @app.route('/')
 def retrieve_menu():
-    print("Inside retrieve_menu function")
+
     menu_ref = db.reference("Menu/Food").get(shallow=True)
     menu = sorted(menu_ref.items(), key=lambda x: x[0])
+    print(menu)
     blobs = bucket.list_blobs(prefix="Food/")
     urls = []
     for blob in blobs:
         url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
         urls.append(url)
+
+
+
     return render_template('index.html', menu=menu, urls=urls)
+
+@app.route('/update_cart', methods=['POST'])
+def update_cart():
+    data = request.get_json()
+
+    # Update the cart in the session
+    item_name = data.get('item')
+    print(f"Updating cart for item: {item_name}")
+
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    session['cart'][item_name] = session['cart'].get(item_name, 0) + 1
+    # Debug print statements
+    print(f"Updated cart: {session['cart']}")
+
+    # Return the updated cart count
+    return jsonify({'cart_count': session['cart'][item_name]})
 
 
 @app.route('/Table/NewTable')
---
+++ static/scripts/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function (){
+    let addToCartButtons = document.querySelectorAll('.add-to-cart');
+    let cartButton = document.getElementById('cart-button');
+
+    addToCartButtons.forEach(function (button){
+        button.addEventListener('click', function (){
+            let itemName = button.getAttribute('data-item');
+            addToCart(itemName);
+        })
+    })
+
+    function addToCart(itemName) {
+        console.log(`Adding ${itemName} to cart`);
+        fetch('/update_cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                item: itemName,
+            }),
+        })
+            .then(response =>response.json())
+            .then(data => {
+                // Update the cart count on the existing Cart button
+                let cartIcon = document.createElement('span');
+                cartIcon.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                    </svg>
+                `;
+                cartButton.innerHTML = '';
+                cartButton.appendChild(cartIcon);
+
+                if (data.cart_count > 0) {
+                    let cartCountSpan = document.createElement('span');
+                    cartCountSpan.innerText = data.cart_count;
+                    cartButton.appendChild(cartCountSpan);
+                }
+            })
+            .catch(error => {
+                console.error('Error: ', error);
+            });
+        console.log('Fetch request sent');
+    }
+});
